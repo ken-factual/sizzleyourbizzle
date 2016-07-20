070ab80f9c330a822a3cf828b5d9dccc84255069
Daniel Li <li.daniel@berkeley.edu>
2016-07-20 14:18:59 -0700

    asdf

--- startbootstrap-sb-admin-1.0.4/js/memes.js
+++ startbootstrap-sb-admin-1.0.4/js/memes.js
@@ -6,7 +6,7 @@ var locations = [
   ['Maroubra Beach', -33.950198, 151.259302, 2]
 ];
 
-var centroid = [-33.92, 151.25];
+var centroid = [-33.92, 151.25]; 
 
 var map = new google.maps.Map(document.getElementById('map'), {
   zoom: 10,
